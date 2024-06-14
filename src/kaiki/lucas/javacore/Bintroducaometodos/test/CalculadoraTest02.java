2361f38ca7655eb00cd9869e359385a5d64529fd
kaikilucas <kaiki.lucasf@gmail.com>
2024-06-14 15:33:03 -0300

    Estudando Métodos com return e parametros

---
+++ src/kaiki/lucas/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,13 @@
+package kaiki.lucas.javacore.Bintroducaometodos.test;
+
+import kaiki.lucas.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+      calculadora.multiplicaDoisNumeros(10, 20);
+
+
+    }
+}
---
+++ src/kaiki/lucas/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,11 @@
+package kaiki.lucas.javacore.Bintroducaometodos.test;
+
+import kaiki.lucas.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+       calculadora.divideDoisNumeros(20, 2);
+        System.out.println(calculadora.divideDoisNumeros(20, 2));
+    }
+}
