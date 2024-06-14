cd8f71f84a54d1eafa420468bb50b99785febfb1
kaikilucas <kaiki.lucasf@gmail.com>
2024-06-14 00:24:29 -0300

    Aula Sobre Métodos

---
+++ src/kaiki/lucas/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,11 @@
+package kaiki.lucas.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+
+    public  void subtraiDoisNumeros(){
+        System.out.println(21-2);
+    }
+}
