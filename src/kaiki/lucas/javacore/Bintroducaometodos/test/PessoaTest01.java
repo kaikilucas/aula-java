2491044895210643b09981912a14e3429c41143b
kaikilucas <kaiki.lucasf@gmail.com>
2024-06-19 21:54:45 -0300

    54 - Orientação Objetos - Modificador de acesso private, get e set pt 01

---
+++ src/kaiki/lucas/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,15 @@
+package kaiki.lucas.javacore.Bintroducaometodos.test;
+
+import kaiki.lucas.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Kaiki");
+        pessoa.setIdade(25);
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+
+
+    }
+}
