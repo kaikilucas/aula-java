c5a3c2d4d9ae54caf32ff62fc47f06245a138c22
kaikilucas <kaiki.lucasf@gmail.com>
2024-06-26 12:51:15 -0300

    Orientação Objetos - Construtores pt 02 - Sobrecarga

--- src/kaiki/lucas/javacore/Dconstrutores/dominio/Anime.java
+++ src/kaiki/lucas/javacore/Dconstrutores/dominio/Anime.java
@@ -5,19 +5,23 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
-public Anime(String nome, String tipo, int episodios, String genero){
-    this.nome = nome;
-    this.tipo = tipo;
-    this.episodios = episodios;
-    this.genero = genero;
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+        this.genero = genero;
+    }
+
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+       this(nome, tipo, episodios,genero);
+        this.estudio = estudio;
+    }
+public Anime (){
+    System.out.println("Dentro do construtor sem argumentos");
 }
-
-public Anime(){
-
-}
-
-
 
     public String getGenero() {
         return genero;
@@ -32,6 +36,7 @@ public Anime(){
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public void setTipo(String tipo) {
