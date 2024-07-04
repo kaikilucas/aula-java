e8f760eb7397bec2785fed77528aa16f365a9338
kaikilucas <kaiki.lucasf@gmail.com>
2024-07-03 23:39:20 -0300

    63 - Orientação Objetos - Bloco de inicialização estático

---
+++ src/kaiki/lucas/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package kaiki.lucas.javacore.Fmodificadorestatico.test;
+
+import kaiki.lucas.javacore.Fmodificadorestatico.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
